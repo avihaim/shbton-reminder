b00dd083d3fd815c2c9c9b3924ab0b54d51e39fe
avihaim <avihaim@gamil.com>
2013-09-26 21:50:58 +0300

    2

---
+++ shbton-reminder-client/platforms/android/bin/PushNotificationApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+d:\java\phonegap\PushNotificationSample\platforms\android\bin\PushNotificationApp-debug-unaligned.apk : \
+d:\java\phonegap\PushNotificationSample\platforms\android\bin\PushNotificationApp.ap_ \
+d:\java\phonegap\PushNotificationSample\platforms\android\bin\classes.dex \
+d:\java\phonegap\PushNotificationSample\platforms\android\libs\cordova-3.0.0.jar \
