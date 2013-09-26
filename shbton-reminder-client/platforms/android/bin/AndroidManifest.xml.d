370b9988e07f103e4960ae55594aca1aa64eefcb
avihaim <avihaim@gamil.com>
2013-09-26 22:12:12 +0300

    1

---
+++ shbton-reminder-client/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+d:\java\phonegap\PushNotificationSample\platforms\android\bin\AndroidManifest.xml : \
+d:\java\phonegap\PushNotificationSample\platforms\android\AndroidManifest.xml \
---
+++ shbton-reminder-client/platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+d:\java\phonegap\PushNotificationSample\platforms\android\gen\com\pushapp\R.java \
+ : d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable\icon.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable-hdpi\icon.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable-hdpi\ic_launcher.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable-ldpi\icon.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable-ldpi\ic_launcher.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable-mdpi\icon.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable-mdpi\ic_launcher.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable-xhdpi\icon.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\drawable-xhdpi\ic_launcher.png \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\layout\main.xml \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\values\strings.xml \
+d:\java\phonegap\PushNotificationSample\platforms\android\res\xml\config.xml \
+d:\java\phonegap\PushNotificationSample\platforms\android\bin\AndroidManifest.xml \
